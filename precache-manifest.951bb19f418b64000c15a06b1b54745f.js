97d234ef9862ff08a7042b2ab3614eb2a1331b08
GiancarloAparicio <zairo2542@gmail.com>
2020-07-31 11:17:54 -0500

    Updates

---
+++ precache-manifest.951bb19f418b64000c15a06b1b54745f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c5c399cc6cdf651fafb11f51b67e9c",
+    "url": "/Login/index.html"
+  },
+  {
+    "revision": "f981756bfe4b37ef2b69",
+    "url": "/Login/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "58105f931ba64fad8bca",
+    "url": "/Login/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "f981756bfe4b37ef2b69",
+    "url": "/Login/static/js/2.ee1b31c0.chunk.js"
+  },
+  {
+    "revision": "58105f931ba64fad8bca",
+    "url": "/Login/static/js/main.14bd4e57.chunk.js"
+  },
+  {
+    "revision": "562edb01e3b5f8d3cc8b",
+    "url": "/Login/static/js/runtime~main.bf058e2e.js"
+  }
+]);
