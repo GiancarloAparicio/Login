59e57abff5b0d176fc7e69396428118322d7ac99
GiancarloAparicio <zairo2542@gmail.com>
2020-08-13 12:48:37 -0500

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Login
 
- Test the page [Demo](https://giancarloaparicio.github.io/Login/)
+Login with gmail authentication, data validation and error messages, created using context, react, bootstrap, firebase
+
+Test the page [Demo](https://giancarloaparicio.github.io/Login/)
+
+### Login user
+
+![Login](Photo-1.jpg)
+
+### Register user
+
+![Login](Photo-2.jpg)
+
+### Verify email
+
+![Login](Photo-3.jpg)
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,74 +1,79 @@
-import React, { useContext } from "react"
-import AppContext from "../Context/AppContext"
-import { closeUser } from "../Firebase/Firebase"
-import EmailCheck from "../includes/EmailCheck"
-import LoaderPage from "../includes/LoaderPage"
+import React, { useContext } from 'react';
+import AppContext from '../Context/AppContext';
+import { closeUser } from '../Firebase/Firebase';
+import EmailCheck from '../includes/EmailCheck';
+import LoaderPage from '../includes/LoaderPage';
 
 //React Router Dom
 //import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 const HomePage = () => {
+	const [state, dispatch] = useContext(AppContext);
 
-    const [state, dispatch] = useContext(AppContext)
+	const closeSession = () => {
+		closeUser(dispatch);
+	};
 
-    const closeSession = () => {
-        closeUser(dispatch)
-    }
+	return (
+		<>
+			<LoaderPage
+				loanding={state?.currentUser === null ? '' : 'hidden'}
+			/>
 
-    return (
-        <>
+			<nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+				<a className='navbar-brand' href='./'>
+					Navbar
+				</a>
+				<button
+					className='navbar-toggler'
+					data-toggle='collapse'
+					data-target='#navbar2'
+					aria-controls='navbarColor01'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span className='navbar-toggler-icon'></span>
+				</button>
 
-            <LoaderPage loanding={(state?.currentUser===null)? "":"hidden"}/>
+				<div className='navbar-collapse collapse' id='navbar2'>
+					<ul className='navbar-nav ml-auto'>
+						<li className='nav-item active'>
+							<a className='nav-link' href='./'>
+								Home
+							</a>
+						</li>
+						<li className='nav-item'>
+							<a className='nav-link' href='./'>
+								Courses
+							</a>
+						</li>
+						<li>
+							<button
+								className='btn btn-danger'
+								onClick={closeSession}
+							>
+								Cerrar Session
+							</button>
+						</li>
+					</ul>
+				</div>
+			</nav>
 
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a className="navbar-brand" href="./">Navbar</a>
-                <button className="navbar-toggler" data-toggle="collapse" data-target="#navbar2" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+			<div className='container'>
+				{state?.currentUser?.emailVerified ? (
+					<h1>Hola, usuario registrado y confirmado</h1>
+				) : (
+					<EmailCheck />
+				)}
 
-                <div className="navbar-collapse collapse" id="navbar2" >
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="./">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="./">Courses</a>
-                        </li>
-                        <li>
-                            <button className="btn btn-danger" onClick={closeSession}>
-                                Cerrar Session
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-            <div className="container">
-
-                {
-                    (state?.currentUser?.emailVerified) ?
-                        <h1>Hola, usuario registrado y confirmado:  {state?.currentUser?.email}</h1>
-                        : <EmailCheck />
-                }
-
-
-
-
-
-
-                {/* <Router>
+				{/* <Router>
                     <Switch>
                         <Route path="/" component={state?.currentUserExists == true ? HomePage : LoginPage} />
                     </Switch>
                 </Router> */}
-
-            </div>
-
-        </>
-
-
-
-    )
-}
+			</div>
+		</>
+	);
+};
 
 export default HomePage;
